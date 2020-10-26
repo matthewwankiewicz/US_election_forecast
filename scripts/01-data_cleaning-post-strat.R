973a0f57aad82f92b4e9cb78b203b1c0edfa7c85
Matthew Wankiewicz <matthew.wankiewicz@hotmail.com>
2020-10-25 20:34:40 -0400

    commit 1

---
+++ scripts/01-data_cleaning-post-strat.R
@@ -0,0 +1,46 @@
+#### Preamble ####
+# Purpose: Prepare and clean the survey data downloaded from IPUMS USA website
+# Author: Alen Mitrovski, Xiaoyan Yang and Matthew Wankiewicz
+# Data: 22 October 2020
+# Contact: matthew.wankiewicz@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: 
+# - Need to have downloaded the ACS data and saved it to inputs/data
+# - Don't forget to gitignore it!
+
+
+#### Workspace setup ####
+library(haven)
+library(tidyverse)
+# Read in the raw data. 
+raw_data <- read_dta("inputs/data/usa_00001.dta"
+                     )
+# Add the labels
+raw_data <- labelled::to_factor(raw_data)
+
+# Just keep some variables that may be of interest (change 
+# this depending on your interests)
+names(raw_data)
+
+reduced_data <- 
+  raw_data %>% 
+  select(region,
+         stateicp,
+         sex, 
+         age, 
+         race, 
+         hispan,
+         marst, 
+         bpl,
+         citizen,
+         educd,
+         labforce,
+         inctot)
+rm(raw_data)
+         
+
+#### What's next? ####
+
+
+
+         
